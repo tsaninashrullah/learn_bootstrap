23abc098446002744a8cc1c67a7ae0c0039d712d
Tsani Nashrullah <tsani.nashrullah@gmail.com>
2016-09-07 12:04:44 +0100

    Make a layout and some function

---
+++ layout/footer.php
@@ -0,0 +1,9 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-1-11-1.js"></script>
+    <!-- BOOTSTRAP MATERIAL DESIGN -->
+    <script src="js/material.js"></script>
+    <script src="js/ripples.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+ </html>
---
+++ layout/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+ <html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Training Bootstrap</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-material-design.css" rel="stylesheet">
+    <link href="css/ripples.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>    
+    <!-- Put Navigation bar here  -->
+    <div class="navbar navbar-warning">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-warning-collapse">
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="javascript:void(0)">Training Bootstrap</a>
+	    </div>
+	    <div class="navbar-collapse collapse navbar-warning-collapse">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="javascript:void(0)">Home</a></li>
+	        <li><a href="javascript:void(0)">Profile</a></li>
+	        <li><a href="javascript:void(0)">Contact</a></li>
+	      </ul>
+	    </div>
+	  </div>
+	</div>
